52cffe3ecbe9f8e2aee8f261596f1d3e7475afe6
Imogen Misso <imogenmisso@hotmail.co.uk>
2018-11-12 17:30:51 +0000

    TEST PASSING WOOO

--- note-list-model.js
+++ note-list-model.js
@@ -8,3 +8,11 @@ exports.NoteList = NoteList;
 NoteList.prototype.add = function(note) {
   this.notes.push(note);
 }
+
+NoteList.prototype.listAll = function() {
+  var string = ""
+  for (var i = 0; i < this.notes.length; i++) {
+    string += (this.notes[i].text + ", ")
+  }
+  return string.slice(0, -2)
+}
--- note-list-test.js
+++ note-list-test.js
@@ -9,8 +9,21 @@ function testNoteListListsNotes(note) {
   var noteList = new NoteList();
   var note = new Note("hi");
   noteList.add(note)
-  assert.isTrue(noteList.notes[0].text == note.text);
+  assert.isTrue(noteList.notes[0].text === note.text);
 };
 
 
 testNoteListListsNotes();
+
+
+function testReturnsAllNotes() {
+  var noteList = new NoteList();
+  var note = new Note("hi");
+  var note2 = new Note("hello");
+  noteList.add(note)
+  noteList.add(note2)
+  console.log(noteList.listAll())
+  assert.isTrue(noteList.listAll() === "hi, hello")
+}
+
+testReturnsAllNotes();
