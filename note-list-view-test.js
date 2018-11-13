2e10939b682069e3cccf4f09f4850733c1685ba5
Imogen Misso <imogenmisso@hotmail.co.uk>
2018-11-13 14:47:51 +0000

    added method getNoteList

--- note-list-view-test.js
+++ note-list-view-test.js
@@ -5,3 +5,14 @@ function testReturnsNoteListModel() {
 }
 
 testReturnsNoteListModel();
+
+
+function testNoteListModelHTMLString() {
+  var noteList = new NoteList();
+  noteList.add("Favourite food: pesto")
+  noteList.add("Favourite drink: seltzer")
+  var noteListView = new NoteListView(noteList);
+  assert.isTrue(noteListView.getNoteList() === '<ul><li><div>Favourite food: pesto</div></li><li><div>Favourite drink: seltzer</div></li></ul>')
+}
+
+testNoteListModelHTMLString();
--- note-list-view.js
+++ note-list-view.js
@@ -2,5 +2,15 @@
   function NoteListView(noteList) {
     this.noteList = noteList;
   }
+
+  NoteListView.prototype.getNoteList = function() {
+    var view = '<ul>'
+    this.noteList.listAll().forEach(function(note) {
+        view += '<li><div>' + note.text + '</div></li>'
+      })
+      view += '</ul>'
+      return view
+  }
+
   exports.NoteListView = NoteListView;
 })(this);
