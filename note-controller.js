17cdc86ae6fc118e793e7f929e4742110425ede5
Imogen Misso <imogenmisso@hotmail.co.uk>
2018-11-13 16:26:57 +0000

    added module function to note controller

--- note-controller.js
+++ note-controller.js
@@ -1,2 +1,18 @@
+(function(exports) {
+  function NoteController(noteList) {
+    this.noteList = noteList;
+    this.noteList.add("Favourite drink: seltzer")
+    this.noteListView = new NoteListView(noteList);
+  }
 
-document.getElementById("app").innerHTML = "howdy";
+  NoteController.prototype.getHTML = function() {
+    document.getElementById("app").innerHTML = this.noteListView.getNoteList();
+  }
+
+
+   exports.NoteController = NoteController;
+})(this);
+
+var noteList = new NoteList()
+var noteController = new NoteController(noteList);
+noteController.getHTML()
