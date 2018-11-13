b56791569ea0d47a7ed9a9857f4a9c4c0aa419cd
Anouska Hopkins <anouskahopkins@gmail.com>
2018-11-13 14:56:18 +0000

    Tested empty notelist and notelist with one note

--- note-list-view-test.js
+++ note-list-view-test.js
@@ -7,12 +7,29 @@ function testReturnsNoteListModel() {
 testReturnsNoteListModel();
 
 
-function testNoteListModelHTMLString() {
+function testNoteListModelHTMLString(textOne, textTwo) {
   var noteList = new NoteList();
-  noteList.add("Favourite food: pesto")
-  noteList.add("Favourite drink: seltzer")
+  noteList.add(textOne);
+  noteList.add(textTwo);
   var noteListView = new NoteListView(noteList);
-  assert.isTrue(noteListView.getNoteList() === '<ul><li><div>Favourite food: pesto</div></li><li><div>Favourite drink: seltzer</div></li></ul>')
+  assert.isTrue(noteListView.getNoteList() === '<ul><li><div>' + textOne + '</div></li><li><div>' + textTwo + '</div></li></ul>');
 }
 
-testNoteListModelHTMLString();
+testNoteListModelHTMLString("Favourite food: pesto", "Favourite drink: seltzer");
+
+function testNoteListModelWithNoNotes() {
+  var noteList = new NoteList();
+  var noteListView = new NoteListView(noteList);
+  assert.isTrue(noteListView.getNoteList() === '<ul></ul>');
+}
+
+testNoteListModelWithNoNotes()
+
+function testNoteListModelWithOneNote(textOne) {
+  var noteList = new NoteList();
+  noteList.add(textOne);
+  var noteListView = new NoteListView(noteList);
+  assert.isTrue(noteListView.getNoteList() === '<ul><li><div>' + textOne + '</div></li></ul>');
+}
+
+testNoteListModelWithNoNotes("Favourite food: pesto")
